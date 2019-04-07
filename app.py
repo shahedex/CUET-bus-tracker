9df52eec7ddd565a17cec6ed75742765459c91f5
shahedex <shahed739@gmail.com>
2019-04-08 01:43:23 +0600

    added openstreetmaps with custom landmark

--- app.py
+++ app.py
@@ -14,22 +14,39 @@ app = Flask(__name__)
 @app.route('/')
 def runpy():
     print("reloading....")
-    return render_template('index.html')
-
-@app.route('/uploaddata')
-def uploaddata():
-    print("uploading data....")
-    people_counter = int(request.args.get('people'))
-    isearthquake = int(request.args.get('earthquake'))
     try:
-        # conn = psycopg2.connect(host="localhost",database="earthquake-detector", user="postgres", password="")
-        # """
+        conn = psycopg2.connect(host="localhost",database="cuetbus", user="postgres", password="")
+        """
         DATABASE_URL = os.environ['DATABASE_URL']
         conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        # """
+        """
         cur = conn.cursor()
-        query = "UPDATE housedata SET peoplein=%s, isearthquake=%s"
-        values = (people_counter, isearthquake)
+        query = "SELECT * FROM padmalatlong"
+        cur.execute(query)
+        results = cur.fetchall()
+        #"""
+        for res in results:
+            print(res)
+        #"""
+        conn.close()
+    except Exception as e:
+        print(e)
+    return render_template('index.html', results=results)
+
+@app.route('/sendlatlong')
+def uploaddata():
+    print("uploading data....")
+    lattitude = request.args.get('lat')
+    longitude = request.args.get('long')
+    try:
+        conn = psycopg2.connect(host="localhost",database="cuetbus", user="postgres", password="")
+        """
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        """
+        cur = conn.cursor()
+        query = "UPDATE padmalatlong SET lat=%s, long=%s"
+        values = (lattitude, longitude)
         cur.execute(query, values)
         conn.commit()
         count = cur.rowcount
@@ -37,7 +54,7 @@ def uploaddata():
         conn.close()
     except Exception as e:
         print(e)
-    return '''{},{}'''.format(people_counter,isearthquake)
+    return '''{},{}'''.format(lattitude,longitude)
 
 if __name__ == '__main__':
     app.debug = True
